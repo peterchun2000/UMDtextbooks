17a62eadb77f5c54fcf420a34f06795be9be56f3
Johnny Chiu <johnnychiu123@gmail.com>
2018-12-01 18:53:54 -0500

    Add files via upload

--- TextbookTrading/tables.sql
+++ TextbookTrading/tables.sql
@@ -1,8 +1,3 @@
-drop database if exists umdlocalhackday2;
-create database if not exists umdlocalhackday2;
-
-use umdlocalhackday2;
-
 CREATE TABLE Person (
 	UserID INT PRIMARY KEY auto_increment,
     Email VARCHAR(100) NOT NULL UNIQUE,
@@ -19,7 +14,7 @@ CREATE TABLE Courses (
     course int(10) not null,
     Primary Key (Department, CourseKey)
 );
-CREate index course on courses(course);
+Create index course on courses(course);
 
 CREATE Table Textbook (
 	Title VARCHAR(255) PRIMARY KEY,
@@ -28,16 +23,11 @@ CREATE Table Textbook (
 	FOREIGN KEY (course) REFERENCES Courses (course)
 );
 CREATE TABLE Inventory (
-	Title VARCHAR(255) NOT NULL ,
+	Title VARCHAR(255),
     Price DOUBLE NOT NULL,
     Wear VARCHAR(10) NOT NULL,
     Comments VARCHAR(255) DEFAULT NULL,
     Seller INT,
     FOREIGN KEY (Seller) REFERENCES Person(UserID),
-    FOREIGN KEY (title) REFERENCES Textbook(Title)
+    FOREIGN KEY (Title) REFERENCES Textbook(Title)
 );
-
-insert courses values ("MATH",141,1), ("CMSC", 132, 2);
-insert textbook values
-("Calculus with concepts","Robert Ellis",1),
-("Calculus with concepts edition 2","Robert Ellis II",1), ("Abstract Java","Nelson Paula-Perez",2);
