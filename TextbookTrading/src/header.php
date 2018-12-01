133ce5a7686230c624c0a3887f40d667a5734732
soccer2k2 <adrtmrber@live.com>
2018-12-01 11:36:08 -0500

    updated style.css format

--- TextbookTrading/src/header.php
+++ TextbookTrading/src/header.php
@@ -1,5 +1,5 @@
 <?php
-$title="Textbook Trading Website";
+$title = "Textbook Trading Website";
 ?>
 <!DOCTYPE html>
 <html>
@@ -9,9 +9,11 @@ $title="Textbook Trading Website";
     <title><?=$title?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="../res/style/style.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 <div id="header">
     <img id="logo" src="../res/img/logo.png" width=50px alt="Textbook Trading Logo">
     <span id="title"><?=$title?></span>
 </div>
+<form action="/youpage.php"> <input type="text" placeholder="Search.." name="search" class="search"> <button type="submit" class="search"><i class="fa fa-search"></i></button> </form>
