4cef2acfe5825648c5997e68eeb160ae10c6490e
soccer2k2 <adrtmrber@live.com>
2018-12-01 11:14:27 -0500

    Updated css location reference

--- TextbookTrading/src/header.php
+++ TextbookTrading/src/header.php
@@ -8,7 +8,7 @@ $title="Textbook Trading Website";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title><?=$title?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--link rel="stylesheet" type="text/css" media="screen" href="main.css" /-->
+    <link rel="stylesheet" type="text/css" media="screen" href="../res/style/style.css"/>
 </head>
 <body>
 <div id="header">
