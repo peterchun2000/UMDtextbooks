0f286b2c1efa0d8528dce97402177bd50320e3ce
Johnny Chiu <johnnychiu123@gmail.com>
2018-12-01 18:56:01 -0500

    You found a golden cookie! 0w0

---
+++ TextbookTrading/CreateSample.sql
@@ -0,0 +1,10 @@
+INSERT INTO Person (Email,PassW,UserName) VALUES('hubba@lul','test','weebLord');
+INSERT INTO Courses VALUES('MATH','141','1');
+INSERT INTO Courses VALUES('Life','101S','5');
+INSERT INTO Courses VALUES('MATH','246','8');
+INSERT INTO Textbook VALUES('Froggers','Mr.Pepe','1');
+INSERT INTO Textbook VALUES('Secrets To Memes','Ms.Pepe','5');
+INSERT INTO Textbook VALUES('Explosions and Frogs','Miss Piggy','1');
+INSERT INTO Textbook VALUES('-_-','Weeb','8');
+INSERT INTO Inventory VALUES('Secrets To Memes','99.99','Take it or beat it',
+'huh','1');
